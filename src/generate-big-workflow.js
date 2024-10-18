0f8c3d3f4664130625d43808920022ec99680fb5
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-10-18 12:58:19 +0200

    chore(workflow): add big workflows

---
+++ src/generate-big-workflow.js
@@ -0,0 +1,48 @@
+import YAML from 'yaml';
+import fs from 'node:fs';
+
+const workflowJSON = {
+    name: 'Big Workflow',
+    on: {
+        workflow_dispatch: {
+            inputs: {}
+        }
+    },
+    jobs: {}
+}
+
+const jobJSON = {
+    'runs-on': 'ubuntu-latest',
+    steps: [
+        {
+            uses: 'actions/checkout@v4'
+        },
+        {
+            name: 'Fake test',
+            shell: 'bash',
+            run: 'echo "Testing..."'
+        },
+        {
+            name: 'Fake lint',
+            shell: 'bash',
+            run: 'echo "Linting..."'
+        },
+        {
+            name: 'Fake bundle',
+            shell: 'bash',
+            run: 'echo "Bundling..."'
+        }
+    ]
+}
+
+for (let i = 1; i <= 10000; i++) {
+    workflowJSON.jobs[`library-${i}`] = {name: `Library ${i}`, ...jobJSON};
+}
+
+const workflowYAML = YAML.stringify(workflowJSON, {aliasDuplicateObjects: false});
+fs.writeFileSync('.github/workflows/big-workflow.yml', workflowYAML);
+
+workflowJSON.name = 'Big Workflow with Aliases';
+
+const workflowYAMLWithAliases = YAML.stringify(workflowJSON);
+fs.writeFileSync('.github/workflows/big-workflow-with-aliases.yml', workflowYAMLWithAliases);
