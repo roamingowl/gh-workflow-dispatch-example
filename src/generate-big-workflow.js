be460b7d2027580bc4cb517e0ec6698503e069ba
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-10-18 13:12:36 +0200

    chore(workflow): add big workflows

--- src/generate-big-workflow.js
+++ src/generate-big-workflow.js
@@ -35,7 +35,7 @@ const jobJSON = {
     ]
 }
 
-for (let i = 1; i <= 1517; i++) {
+for (let i = 1; i <= 1516; i++) {
     workflowJSON.jobs[`library-${i}`] = {name: `Library ${i}`, ...jobJSON};
 }
 
